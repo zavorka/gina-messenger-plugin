d9be43aa77adddb63184e84c0787cb5485852b52
Roman Beránek <beranek@ginasystem.com>
2014-06-25 15:24:05 +0200

    Added navigation and refactored the name of this plugin.

---
+++ www/ginaplugin.js
@@ -0,0 +1,36 @@
+var ginaplugin = {
+    
+    /**
+     * Get device IMEI
+     * @param {function(string)} onSuccess Called with one param - string containing IMEI of the device
+     * @param {function(string)} onError Called with one param - string containing error
+     */                    
+    getIMEI: function(onSuccess, onError) {
+        cordova.exec(onSuccess, onError, 'DeviceInfo', 'getIMEI', []);    
+    },
+    
+    getImageFromZip: function(onSuccess, onError, zipPath, entryPath) {
+    	cordova.exec(onSuccess, onError, 'DeviceInfo', 'getImageFromZip', [zipPath, entryPath]);
+    }
+
+    doNavigate: function(onSuccess, onError)
+            if !(device?) {
+                window.open("http://maps.google.com/maps?daddr=" + lat + "," + lon);
+                onSuccess?();
+            }
+            # Use a plugin to execute an Intent on Android.
+            else if (device.platform === "Android")
+                cordova.exec(onSuccess, onError, 'PhoneNavigator', 'doNavigate', [lat, lon]);
+            else if (device.platform == "iOS") {
+                window.location = "maps:daddr=" + lat + "," + lon;
+                onSuccess?();
+            }
+            else {
+                console.log("Unknown platform.");
+                window.location = "http://maps.google.com/maps?daddr=" + lat + "," + lon;
+                onSuccess();
+            }
+}
+
+
+module.exports = ginaplugin;
