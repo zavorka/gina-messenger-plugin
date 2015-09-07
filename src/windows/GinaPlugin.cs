9b5099f22c3ac43de9ea89c9202851114fb37407
Jan Knezik <knezik@ginasystem.com>
2015-09-07 13:17:14 +0200

    Added imei detection for windows

---
+++ src/windows/GinaPlugin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace WPCordovaClassLib.Cordova.Commands
+{
+    public class GinaPlugin : BaseCommand
+    {
+        public void getIMEI()
+        {
+            PluginResult result;           
+            object uniqueId;
+            var imei = string.Empty;
+            
+            if (DeviceExtendedProperties.TryGetValue("DeviceUniqueId", out uniqueId))
+                imei = BitConverter.ToString((byte[]) uniqueId).Replace("-", string.Empty);
+            
+            if (upperCase != "")
+            {
+                result = new PluginResult(PluginResult.Status.OK, imei);
+            } else
+            {
+                result = new PluginResult(PluginResult.Status.ERROR, imei);
+            }
+
+            DispatchCommandResult(result);
+        }
+    }
+}
