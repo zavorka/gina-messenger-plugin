d76b72b68afec8986c82a91b4b9da2870ce56a9f
Jan Knezik <knezik@ginasystem.com>
2015-09-08 20:58:09 +0200

    Minor change

--- src/windows/GinaPluginProxy.js
+++ src/windows/GinaPluginProxy.js
@@ -1,6 +1,6 @@
 cordova.commandProxy.add("GinaPlugin", {
     getIMEI : function(successCallback, errorCallback) {    
-        var res = GinaPlugin.GinaPluginImpl.getIMEI();
+        var res = ginaplugin.GinaPluginImpl.getIMEI();
         
         if (res.indexOf("Error") == 0) {
             errorCallback(res);
